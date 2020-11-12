73e8cf0d36028c8e18b06c047ee1deaca99d0803
jangle <shubhamjangale0000@gmail.com>
2020-11-12 07:46:34 +0530

    Added : Added functionality to print the nth Harmonic of a number

---
+++ repitition2.js
@@ -0,0 +1,7 @@
+//Finding the nth Harmonic Number
+var num = process.argv[2];
+var result = 0;
+for (var i = 1; i <= num; i++) {
+    result += 1 / i;
+}
+console.log("Nth Harmonic number is " + result);
